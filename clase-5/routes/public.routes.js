3dfaa3db526b1da091adaed0449368c03955bb0a
josuemartinez <josuecjmm@gmail.com>
2024-02-27 18:50:50 -0600

    clase 5 y tarea

--- clase-5/routes/public.routes.js
+++ clase-5/routes/public.routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const { getIndex } = require('../controller/public.controller');
+const { getIndex, getUserInfo, getTest, postProposal } = require('../controller/public.controller');
 const router = express.Router();
 
 router.get('/', getIndex);
+router.get('/user', getUserInfo);
+router.get('/test', getTest);
+router.post('/proposal', postProposal);
 
 module.exports = {
     routes: router,
